a5f63bd43958ad3955d4dfa370536e90a54e9eb0
kira924age <kira924age@gmail.com>
2024-08-29 12:26:40 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Tooltip } from 'react-tooltip'
+
+import foxModel from './assets/Fox.glb'
+import "@google/model-viewer";
+import './App.css'
+
+const content: JSX.Element = (<div>
+    <div>
+    Model License Information
+    </div>
+    <ul>
+      <li>CC0: Low poly fox by PixelMannen</li>
+      <li>CC-BY 4.0: Rigging and animation by @tomkranis on Sketchfab</li>
+      <li>glTF conversion by @AsoboStudio and @scurest</li>
+    </ul>
+  </div>
+)
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Tooltip anchorSelect="#clickable" clickable style={{ backgroundColor: "white", color: "#222" }}>
+      {content}</Tooltip>
+      <div className="heading-container">
+        <div>model-viewer demo</div>
+        <a id="clickable">info</a>
+      </div>
+      <div className="model-viewer-container">
+        <model-viewer src={foxModel} alt="A 3D model of a fox" ar auto-rotate camera-controls with-credentials={true} autoplay />
+      </div>
+    </>
+  )
+}
+
+export default App
